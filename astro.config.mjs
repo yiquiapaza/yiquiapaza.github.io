58d4d36580b84524a45be8410e2f888c3d924c6d
Yhonatan Jesus <yiquiapaza@gmail.com>
2023-09-26 01:10:30 -0500

    fix: update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-        site: "https://yiquiapaza.github.io,"
+  site: "https://yiquiapaza.github.io",
 });
