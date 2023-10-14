88886d8ab229755040fcfff2fe356f3e314d1f19
Yhonatan Jesus <yiquiapaza@gmail.com>
2023-10-14 13:04:10 -0500

    feat: using public path for asset

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,26 @@
     <nav class="container relative mx-auto p-6">
         <div class="flex items-center justify-between">
             <div class="flex items-center space-x-20">
-                <img :src="`../../public/yhonatan.svg`" alt="" />
-                <div class="hidden space-x-8 font-bold text-lg md:flex">
-                    <a href="#" class="hover:text-palermo5 text-palermo2 font-poppins"
+                <img src="/yhonatan.svg" alt="" />
+                <div class="hidden space-x-8 text-lg font-bold md:flex">
+                    <a
+                        href="#"
+                        class="font-poppins text-palermo2 hover:text-palermo5"
                         >Home</a
                     >
-                    <a href="#" class="hover:text-palermo5 text-palermo2 font-poppins"
+                    <a
+                        href="#"
+                        class="font-poppins text-palermo2 hover:text-palermo5"
                         >Publications</a
                     >
-                    <a href="#" class="hover:text-palermo5 text-palermo2 font-poppins"
+                    <a
+                        href="#"
+                        class="font-poppins text-palermo2 hover:text-palermo5"
                         >Projects</a
                     >
-                    <a href="#" class="hover:text-palermo5 text-palermo2 font-poppins"
+                    <a
+                        href="#"
+                        class="font-poppins text-palermo2 hover:text-palermo5"
                         >CV</a
                     >
                 </div>
