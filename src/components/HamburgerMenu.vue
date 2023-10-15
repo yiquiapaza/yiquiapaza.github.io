2c639d05be5d0fb6a82f1867b397f5b352129dfe
Yhonatan Jesus <yiquiapaza@gmail.com>
2023-10-15 01:13:53 -0500

    feat: add HamburgerMenu and config

---
+++ src/components/HamburgerMenu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div
+        id="menu"
+        class="absolute bottom-0 left-0 top-0 hidden min-h-screen w-full flex-col space-y-3 self-end py-1 pl-12 pt-40 text-lg uppercase text-palermo2"
+    >
+        <div>
+            <a class="hover:text-palermo5">Home</a>
+        </div>
+        <div>
+            <a class="hover:text-palermo5">Publications</a>
+        </div>
+        <div>
+            <a class="hover:text-palermo5">Projects</a>
+        </div>
+        <div>
+            <a class="hover:text-palermo5">CV</a>
+        </div>
+    </div>
+</template>
+
+<script></script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,50 @@
 <template>
-    <nav class="container relative mx-auto p-6">
-        <div class="flex items-center justify-between">
-            <div class="flex items-center space-x-20">
-                <img src="/yhonatan.svg" alt="" />
-                <div class="hidden space-x-8 text-lg font-bold md:flex">
-                    <a
-                        href="#"
-                        class="font-poppins text-palermo2 hover:text-palermo5"
-                        >Home</a
-                    >
-                    <a
-                        href="#"
-                        class="font-poppins text-palermo2 hover:text-palermo5"
-                        >Publications</a
-                    >
-                    <a
-                        href="#"
-                        class="font-poppins text-palermo2 hover:text-palermo5"
-                        >Projects</a
-                    >
-                    <a
-                        href="#"
-                        class="font-poppins text-palermo2 hover:text-palermo5"
-                        >CV</a
-                    >
+    <div class="container mx-auto max-w-6xl px-6 py-12">
+        <nav class="container relative mx-auto p-6">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-20">
+                    <img src="/yhonatan.svg" alt="" />
+                    <div class="hidden space-x-8 text-lg md:flex">
+                        <a
+                            href="#"
+                            class="font-poppins text-palermo2 hover:text-palermo5"
+                            >Home</a
+                        >
+                        <a
+                            href="#"
+                            class="font-poppins text-palermo2 hover:text-palermo5"
+                            >Publications</a
+                        >
+                        <a
+                            href="#"
+                            class="font-poppins text-palermo2 hover:text-palermo5"
+                            >Projects</a
+                        >
+                        <a
+                            href="#"
+                            class="font-poppins text-palermo2 hover:text-palermo5"
+                            >CV</a
+                        >
+                    </div>
                 </div>
+                <div class="right-0 top-0 md:hidden">
+                    <button
+                        id="menu-btn"
+                        type="button"
+                        class="hamburger z-40 block focus:outline-none md:hidden"
+                    >
+                        <span class="hamburger-top"></span>
+                        <span class="hamburger-middle"></span>
+                        <span class="hamburger-bottom"></span>
+                    </button>
+                </div>
+                <hamburger-menu></hamburger-menu>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HamburgerMenu from './HamburgerMenu.vue';
+
+//TODO: Create script for hamburger menu when is open change color and create card
+</script>
