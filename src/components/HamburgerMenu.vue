58c79859e64257f2fe54afb0eacb91a8eb33f1c0
Yhonatan Jesus <yiquiapaza@gmail.com>
2023-10-20 21:22:43 -0500

    feat: change icon

--- src/components/HamburgerMenu.vue
+++ src/components/HamburgerMenu.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, reactive } from 'vue';
+import { reactive } from 'vue';
 const props = defineProps({
     urlName: { type: String, required: true },
 });
