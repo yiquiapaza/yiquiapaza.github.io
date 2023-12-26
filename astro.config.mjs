e1336fe6c273b8b796b11261d61e1fa61852015e
Yhonatan Jesus <yiquiapaza@gmail.com>
2023-12-26 14:33:44 -0500

    feat: add images for each publication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue(), tailwind()],
+    integrations: [tailwind()],
     site: 'https://yiquiapaza.github.io',
 });
