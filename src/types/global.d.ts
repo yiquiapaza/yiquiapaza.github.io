bb1bb1805e838185262be55fe2c1f33f8dabfc82
Yhonatan Jesus <yiquiapaza@gmail.com>
2025-05-02 22:42:54 -0500

    fix: remove deprecated style and refactor data

---
+++ src/types/global.d.ts
@@ -0,0 +1,13 @@
+export interface publication {
+  title: string;
+  event: string;
+  authors: string[];
+  image: string;
+  year: number;
+  doi: string;
+  url: string;
+  pdf: string;
+  video: string;
+  alt: string;
+  acronym: string;
+}
